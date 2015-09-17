89221d08262aea262cd79a3307a543725e5628d3
Ricky <ricky.neil@thalesgroup.com.au>
2015-09-17 10:13:17 +1000

    Changed logFile to auto_ptr and added a comment explaining why. Removed the `\n` from the logging entry since log file will add it automatically.

--- SimpleLog/main.cpp
+++ SimpleLog/main.cpp
@@ -1,6 +1,10 @@
 #include "eventlog.h"
 
-static EventLog* logFile = nullptr; /**< LogFile used */
+/** 
+ * auto_ptr is used so that when this log file pointer
+ * goes out of scope it auto calls the destructor and flushes.
+ **/
+static std::auto_ptr<EventLog> logFile;
 
 int
 main( int argc, char* argv[] )
@@ -11,9 +15,7 @@ main( int argc, char* argv[] )
 
 	logFile->SetEventLevel( (EventLevel) EL_WARN );
 
-	logFile->Write( EL_CRITICAL, TEXT("Logging Test Success.\n") );
-
-	EventLog::CloseAll();
+	logFile->Write( EL_CRITICAL, TEXT("Logging Test Success.") );
 
 	return error;
 }
