e971fae9a8b508444608dba69c52c251cb5f74cf
Ricky <ricky.neil@thalesgroup.com.au>
2015-09-17 13:17:59 +1000

    Added more tests designed to work the auto_destroy and log_handle functionality.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # SimpleLog
 Windows Multi-Thread Safe Lite Logging
+
+## Usage
+Include the `eventlog.h` file into all `.cpp` files you want to run the event log in.
+
+Call the EventLog initialisation function:
+
+```C++
+EventLog::InitialiseLog(
+	const TSTRING filename,
+	const TSTRING path,
+	const EventLevel level,
+	const int wait,
+	const int maxqueue )
+```
+
+store the result in an `auto_ptr<EventLog>` with the scope of your choosing.
+
+Write to the log exactly like `printf` except with a logging level at the front:
+
+ - EL_DEBUG
+ - EL_INFO
+ - EL_WARN
+ - EL_ERROR
+ - EL_CRITICAL
+
+```C++
+void
+EventLog::Write( const EventLevel level, const TCHAR* const format, ... )
+
+
+## TODO
+Tests!
--- SimpleLog/main.cpp
+++ SimpleLog/main.cpp
@@ -4,7 +4,40 @@
  * auto_ptr is used so that when this log file pointer
  * goes out of scope it auto calls the destructor and flushes.
  **/
-static std::auto_ptr<EventLog> logFile;
+static std::auto_ptr<LogHandle> logFile;
+
+void
+makeLog1()
+{
+	std::auto_ptr<LogHandle> logFile1 = EventLog::InitialiseLog( TEXT("SimpleLog_Test.log") );
+
+	logFile->Write( EL_CRITICAL, TEXT("Logging Test Success %d."), 1 );
+}
+
+void
+makeLog2()
+{
+	std::auto_ptr<LogHandle> logFile2 = EventLog::InitialiseLog( TEXT("SimpleLog_Test.log") );
+
+	logFile->Write( EL_CRITICAL, TEXT("Logging Test Success %d."), 2 );
+}
+
+void
+makeLog3()
+{
+	std::auto_ptr<LogHandle> logFile3 = EventLog::InitialiseLog( TEXT("SimpleLog_Test.log") );
+
+	logFile->Write( EL_CRITICAL, TEXT("Logging Test Success %d."), 3 );
+}
+
+void
+makeLog4()
+{
+	std::auto_ptr<LogHandle> logFile4 = EventLog::InitialiseLog( TEXT("SimpleLog_Test.log") );
+
+	logFile->Write( EL_CRITICAL, TEXT("Logging Test Success %d."), 4 );
+}
+
 
 int
 main( int argc, char* argv[] )
@@ -14,6 +47,13 @@ main( int argc, char* argv[] )
 	logFile = EventLog::InitialiseLog( TEXT("SimpleLog_Test.log") );
 
 	logFile->SetEventLevel( (EventLevel) EL_WARN );
+
+	makeLog1();
+	makeLog2();
+	makeLog3();
+	makeLog4();
+
+	
 
 	logFile->Write( EL_CRITICAL, TEXT("Logging Test Success.") );
 
